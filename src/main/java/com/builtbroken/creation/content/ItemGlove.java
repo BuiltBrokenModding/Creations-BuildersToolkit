1baaadc8a1082b6b5cf7c6ed2396d38db598593c
Robert S <rseifert.phone@gmail.com>
2015-01-26 04:03:16 -0500

    Added mode changing to glove using middle mouse wheel

--- src/main/java/com/builtbroken/creation/content/ItemGlove.java
+++ src/main/java/com/builtbroken/creation/content/ItemGlove.java
@@ -3,32 +3,45 @@ package com.builtbroken.creation.content;
 import com.builtbroken.creation.Creation;
 import com.builtbroken.creation.selection.Selection;
 import com.builtbroken.creation.selection.SelectionHandler;
+import com.builtbroken.mc.core.Engine;
+import com.builtbroken.mc.core.network.IPacketIDReceiver;
+import com.builtbroken.mc.core.network.IPacketReceiver;
+import com.builtbroken.mc.core.network.packet.PacketPlayerItem;
+import com.builtbroken.mc.core.network.packet.PacketType;
 import com.builtbroken.mc.lib.transform.vector.Location;
-import com.builtbroken.mc.lib.transform.vector.Pos;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import io.netty.buffer.ByteBuf;
 import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.client.event.MouseEvent;
+import net.minecraftforge.common.MinecraftForge;
 
 /**
  * Created by robert on 1/25/2015.
  */
-public class ItemGlove extends Item
+public class ItemGlove extends Item implements IPacketIDReceiver
 {
-    protected int mode = 0;
+    public static final int MODE_PACKET = 0;
+
+    protected int max_mode = 1;
+    protected int min_mode = 0;
 
     public ItemGlove()
     {
         this.setMaxStackSize(1);
+        MinecraftForge.EVENT_BUS.register(this);
     }
 
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
     {
-        if(world != null && player != null)
+        if (world != null && player != null)
         {
             Location location = new Location(world, x, y, z);
             Block block = location.getBlock();
@@ -39,7 +52,9 @@ public class ItemGlove extends Item
                 {
                     switch (stack.getItemDamage())
                     {
-                        case 0: handelSelection(stack, player, location); break;
+                        case 0:
+                            handelSelection(stack, player, location);
+                            break;
                     }
                 }
                 return true;
@@ -55,7 +70,7 @@ public class ItemGlove extends Item
     {
         Selection select = SelectionHandler.getSelection(player.getUniqueID());
 
-        if(player.isSneaking())
+        if (player.isSneaking())
         {
             select.setPointOne(location.toVector3());
             if (Creation.isDevEnv())
@@ -67,5 +82,79 @@ public class ItemGlove extends Item
             if (Creation.isDevEnv())
                 player.addChatComponentMessage(new ChatComponentText(("Point Two: " + select)));
         }
+    }
+
+    @SubscribeEvent
+    public void mouseHandler(MouseEvent e)
+    {
+        if (e.dwheel != 0)
+        {
+            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
+            if (player.isSneaking())
+            {
+                ItemStack stack = player.getCurrentEquippedItem();
+                if(stack != null && stack.getItem() == this)
+                {
+                    cycleMode(stack, player, e.dwheel / 120);
+                    e.setCanceled(true);
+                }
+            }
+        }
+    }
+
+    /**
+     * Causes the mode to cycle
+     * @param stack - itemstack
+     * @param player - player
+     * @param delta - change in mode
+     */
+    protected void cycleMode(ItemStack stack, EntityPlayer player, int delta)
+    {
+        int n = getMode(stack) + delta;
+        if(n > max_mode)
+            n = min_mode;
+        if(n < min_mode)
+            n = max_mode;
+
+        if(getMode(stack) != n)
+        {
+            setMode(stack, n);
+            Engine.instance.packetHandler.sendToServer(new PacketPlayerItem(player, MODE_PACKET, n));
+        }
+    }
+
+    protected int getMode(ItemStack stack)
+    {
+        if(stack.getTagCompound() != null)
+        {
+            return stack.getTagCompound().getInteger("mode");
+        }
+        return min_mode;
+    }
+
+    protected void setMode(ItemStack stack, int mode)
+    {
+        if(stack.getTagCompound() == null)
+            stack.setTagCompound(new NBTTagCompound());
+
+        stack.getTagCompound().setInteger("mode", mode);
+    }
+
+    @Override
+    public boolean read(ByteBuf buf, int id, EntityPlayer player, PacketType packet)
+    {
+        System.out.println("Packet: " + packet +"   ID: " + id);
+        if(packet instanceof PacketPlayerItem)
+        {
+            if(id == MODE_PACKET)
+            {
+                int slot = ((PacketPlayerItem) packet).slotId;
+                ItemStack stack = player.inventory.getStackInSlot(slot);
+                setMode(stack, buf.readInt());
+                player.addChatComponentMessage(new ChatComponentText("Mode set to " + getMode(stack)));
+                return true;
+            }
+        }
+        return false;
     }
 }
