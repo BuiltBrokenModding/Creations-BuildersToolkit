23c59261cbfaafb37ae45f05a9ea291c232ff38c
Robert S <rseifert.phone@gmail.com>
2015-02-14 06:01:08 -0500

    [Untested] Coded selection deletion for builder's glove

--- src/main/java/com/builtbroken/creation/content/ItemGlove.java
+++ src/main/java/com/builtbroken/creation/content/ItemGlove.java
@@ -4,36 +4,39 @@ import com.builtbroken.creation.Creation;
 import com.builtbroken.creation.selection.Selection;
 import com.builtbroken.creation.selection.SelectionHandler;
 import com.builtbroken.mc.api.items.IModeItem;
-import com.builtbroken.mc.core.network.IPacketIDReceiver;
-import com.builtbroken.mc.core.network.packet.PacketPlayerItem;
-import com.builtbroken.mc.core.network.packet.PacketType;
+import com.builtbroken.mc.core.registry.implement.IPostInit;
 import com.builtbroken.mc.lib.helper.LanguageUtility;
 import com.builtbroken.mc.lib.transform.vector.Location;
-import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import com.builtbroken.mc.lib.transform.vector.Pos;
+import com.builtbroken.mc.prefab.inventory.InventoryUtility;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-import io.netty.buffer.ByteBuf;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.MouseEvent;
-import net.minecraftforge.common.MinecraftForge;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by robert on 1/25/2015.
  */
-public class ItemGlove extends Item implements IModeItem.IModeScrollItem
+public class ItemGlove extends Item implements IModeItem.IModeScrollItem, IPostInit
 {
-    protected int max_mode = 1;
-    protected int min_mode = 0;
+    protected int max_mode = 2;
+    protected int max_energy = 10000;
+    protected int energy_cost_delete = 5;
 
     public ItemGlove()
     {
@@ -43,37 +46,163 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem
         this.setUnlocalizedName(Creation.PREFIX + "glove");
     }
 
-    @Override @SideOnly(Side.CLIENT)
+    @Override
+    public void onPostInit()
+    {
+        GameRegistry.addShapedRecipe(new ItemStack(Creation.glove), "lGl", "geg", "lll", 'l', Items.leather, 'e', Items.ender_pearl, 'g', Items.gold_nugget, 'G', Blocks.glass);
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean b)
     {
         //TODO translate based on mode type
         lines.add(LanguageUtility.getLocal(getUnlocalizedName() + ".info.name"));
-        lines.add(LanguageUtility.getLocal(getUnlocalizedName() + ".mode.name") +": " + LanguageUtility.getLocal(getUnlocalizedName() + ".mode." + getMode(stack)+ ".name"));
+        lines.add(LanguageUtility.getLocal(getUnlocalizedName() + ".mode.name") + ": " + LanguageUtility.getLocal(getUnlocalizedName() + ".mode." + getMode(stack) + ".name"));
     }
+
+    @Override
+    public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack)
+    {
+        return true;
+    }
+
+    @Override
+    public boolean onLeftClickEntity(ItemStack stack, EntityPlayer player, Entity entity)
+    {
+        return false;
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
+    {
+        return stack;
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack stack)
+    {
+        int mode = getMode(stack);
+        if (mode == GloveModes.DELETE.ordinal())
+        {
+            return EnumAction.bow;
+        }
+        return EnumAction.none;
+    }
+
+    @Override
+    public int getMaxItemUseDuration(ItemStack stack)
+    {
+        int mode = getMode(stack);
+        if (mode == GloveModes.DELETE.ordinal())
+        {
+            return 10000;
+        }
+        return 0;
+    }
+
+    @Override
+    public void onPlayerStoppedUsing(ItemStack stack, World world, EntityPlayer player, int ticks)
+    {
+        int mode = getMode(stack);
+        if (mode == GloveModes.DELETE.ordinal())
+        {
+            //Consume energy after stop using to prevent client side sync issues
+            consumeEnergy(stack, energy_cost_delete * ticks, true);
+        }
+    }
+
+    @Override
+    public void onUsingTick(ItemStack stack, EntityPlayer player, int count)
+    {
+        int mode = getMode(stack);
+        if (mode == GloveModes.DELETE.ordinal())
+        {
+            Selection select = SelectionHandler.getSelection(player.getUniqueID());
+
+            if (select != null && select.isValid() && consumeEnergy(stack, energy_cost_delete * count, false))
+            {
+                List<Pos> l = select.getLocationsWithin(new Location(player), 1, 50);
+                if (count % 5 == 0)
+                {
+                    if (l != null && l.size() > 0 && l.get(0) != null)
+                    {
+                        Pos pos = l.get(0);
+                        if(pos.distance(new Pos(player)) > 100)
+                        {
+                            if (!player.worldObj.isRemote)
+                            {
+                                Block block = pos.getBlock(player.worldObj);
+                                if(block != null && !pos.isAirBlock(player.worldObj))
+                                {
+                                    block.onBlockDestroyedByPlayer(player.worldObj, pos.xi(), pos.yi(), pos.zi(), pos.getBlockMetadata(player.worldObj));
+                                    ArrayList<ItemStack> items =  block.getDrops(player.worldObj, pos.xi(), pos.yi(), pos.zi(), pos.getBlockMetadata(player.worldObj), 0);
+                                    pos.setBlockToAir(player.worldObj);
+                                    if(!player.capabilities.isCreativeMode)
+                                    {
+                                        for(ItemStack s : items)
+                                        {
+                                            if(!player.inventory.addItemStackToInventory(s))
+                                            {
+                                                InventoryUtility.dropItemStack(new Location(player), s);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                //TODO player visuals and audio effects of removing a block
+                            }
+                        }
+                        else
+                        {
+                            player.addChatComponentMessage(new ChatComponentText(LanguageUtility.getLocal(getUnlocalizedName() + ".info.delete.noblockdistance.name")));
+                            player.stopUsingItem();
+                        }
+                    }
+                    else
+                    {
+                        player.addChatComponentMessage(new ChatComponentText(LanguageUtility.getLocal(getUnlocalizedName() + ".info.delete.noblocks.name")));
+                        player.stopUsingItem();
+                    }
+                }
+            }
+            else
+            {
+                if (!player.worldObj.isRemote)
+                {
+                    if (select == null)
+                    {
+                        player.addChatComponentMessage(new ChatComponentText(LanguageUtility.getLocal(getUnlocalizedName() + ".info.noselection.name")));
+                    }
+                    else if (!select.isValid())
+                    {
+                        player.addChatComponentMessage(new ChatComponentText(LanguageUtility.getLocal(getUnlocalizedName() + ".info.invalidselection.name")));
+                    }
+                }
+                player.stopUsingItem();
+            }
+        }
+    }
+
 
     @Override
     public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int x, int y, int z, int side, float hitX, float hitY, float hitZ)
     {
-        if (world != null && player != null)
+        if (getMode(stack) != GloveModes.NONE.ordinal() && world != null && player != null)
         {
             Location location = new Location(world, x, y, z);
             Block block = location.getBlock();
 
             if (block != null && !location.isAirBlock())
             {
-                if (!world.isRemote)
+                int mode = getMode(stack);
+                if (mode == GloveModes.SELECTION.ordinal())
                 {
-                    switch (stack.getItemDamage())
-                    {
-                        case 0:
-                            //Leave empty to say no mode
-                            break;
-                        case 1:
-                            handelSelection(stack, player, location);
-                            break;
-                    }
+                    handelSelection(stack, player, location);
+                    return true;
                 }
-                return true;
             }
         }
         return false;
@@ -84,19 +213,22 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem
      */
     public void handelSelection(ItemStack stack, EntityPlayer player, Location location)
     {
-        Selection select = SelectionHandler.getSelection(player.getUniqueID());
+        if (!location.world().isRemote)
+        {
+            Selection select = SelectionHandler.getSelection(player.getUniqueID());
 
-        if (player.isSneaking())
-        {
-            select.setPointOne(location.toVector3());
-            if (Creation.isDevEnv())
-                player.addChatComponentMessage(new ChatComponentText(("Point One: " + select)));
-        }
-        else
-        {
-            select.setPointTwo(location.toVector3());
-            if (Creation.isDevEnv())
-                player.addChatComponentMessage(new ChatComponentText(("Point Two: " + select)));
+            if (player.isSneaking())
+            {
+                select.setPointOne(location.toVector3());
+                if (Creation.isDevEnv())
+                    player.addChatComponentMessage(new ChatComponentText(("Point One: " + select)));
+            }
+            else
+            {
+                select.setPointTwo(location.toVector3());
+                if (Creation.isDevEnv())
+                    player.addChatComponentMessage(new ChatComponentText(("Point Two: " + select)));
+            }
         }
     }
 
@@ -105,8 +237,8 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem
     {
         int n = getMode(stack) + delta;
         if (n > max_mode)
-            n = min_mode;
-        if (n < min_mode)
+            n = 0;
+        if (n < 0)
             n = max_mode;
 
         return n;
@@ -119,7 +251,7 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem
         {
             return stack.getTagCompound().getInteger("mode");
         }
-        return min_mode;
+        return 0;
     }
 
     @Override
@@ -129,5 +261,50 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem
             stack.setTagCompound(new NBTTagCompound());
 
         stack.getTagCompound().setInteger("mode", mode);
+    }
+
+    public int getEnergy(ItemStack stack)
+    {
+        if (stack.getTagCompound() != null)
+        {
+            return stack.getTagCompound().getInteger("en");
+        }
+        return 0;
+    }
+
+    public void setEnergy(ItemStack stack, int e)
+    {
+        if (stack.getTagCompound() == null)
+            stack.setTagCompound(new NBTTagCompound());
+
+        stack.getTagCompound().setInteger("en", e);
+    }
+
+    public boolean consumeEnergy(ItemStack stack, int e, boolean doConsume)
+    {
+        if (getEnergy(stack) >= e)
+        {
+            if (doConsume)
+                setEnergy(stack, getEnergy(stack) - e);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean showDurabilityBar(ItemStack stack)
+    {
+        return getEnergy(stack) > 0;
+    }
+
+    @Override
+    public double getDurabilityForDisplay(ItemStack stack)
+    {
+        return getEnergy(stack) / max_energy;
+    }
+
+    public static enum GloveModes
+    {
+        NONE, SELECTION, DELETE;
     }
 }
--- src/main/java/com/builtbroken/creation/selection/Selection.java
+++ src/main/java/com/builtbroken/creation/selection/Selection.java
@@ -1,16 +1,19 @@
 package com.builtbroken.creation.selection;
 
-import java.util.LinkedList;
-import java.util.List;
-
+import com.builtbroken.creation.schematic.Schematic;
+import com.builtbroken.mc.lib.transform.sorting.Vector3DistanceComparator;
+import com.builtbroken.mc.lib.transform.vector.Location;
 import com.builtbroken.mc.lib.transform.vector.Pos;
 import net.minecraft.block.Block;
 import net.minecraft.world.World;
-import com.builtbroken.creation.schematic.Schematic;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * 3D box area
- * 
+ *
  * @author Darkguardsman
  */
 public class Selection
@@ -18,7 +21,7 @@ public class Selection
     private Pos pointOne; // left click
     private Pos pointTwo; // right click
     private Pos lowPoint;
-    private Pos highPoint;   
+    private Pos highPoint;
 
     public Selection(Pos one, Pos two)
     {
@@ -26,15 +29,16 @@ public class Selection
         pointTwo = two;
         recalc();
     }
-    
+
     /**
      * Loads up the cube area as a schematic
+     *
      * @param world - world to load from
      * @return new instance of the schematic matching the blocks in the area, only returns null if the world is null
      */
     public Schematic asSchematic(World world)
     {
-        if(world != null)
+        if (world != null)
         {
             Schematic sch = new Schematic();
             sch.load(world, this);
@@ -42,91 +46,105 @@ public class Selection
         }
         return null;
     }
-    
-    /** Gets the 3D area of the selection */
+
+    /**
+     * Gets the 3D area of the selection
+     */
     public int getArea()
     {
         return getXLength() * getYLength() * getZLength();
     }
-    
-    /** Gets x length of the area */
+
+    /**
+     * Gets x length of the area
+     */
     public int getXLength()
     {
         return highPoint.xi() - lowPoint.xi() + 1;
     }
 
-    /** Gets y length of the area */
+    /**
+     * Gets y length of the area
+     */
     public int getYLength()
     {
         return highPoint.yi() - lowPoint.yi() + 1;
     }
 
-    /** Gets z length of the area */
+    /**
+     * Gets z length of the area
+     */
     public int getZLength()
     {
         return highPoint.zi() - lowPoint.zi() + 1;
     }
-    
-    /** Gets the size of the cube as a Pos */
+
+    /**
+     * Gets the size of the cube as a Pos
+     */
     public Pos getSize()
     {
         return new Pos(getXLength(), getYLength(), getZLength());
     }
-    
+
     /**
      * Returns whether or not this Cube object is a valid cube.
      * Checks to ensure that neither internal Postors are null, and all Y values are above zero.
+     *
      * @return
      */
     public boolean isValid()
     {
         if (pointOne == null || pointTwo == null)
             return false;
-        
+
         return pointTwo.yi() >= 0 && pointOne.yi() >= 0;
     }
-    
+
     private void recalc()
     {
         if (!isValid())
             return; // pointless if both arnt set.
-        
+
         lowPoint = new Pos(Math.min(pointOne.xi(), pointTwo.xi()), Math.min(pointOne.yi(), pointTwo.yi()), Math.min(pointOne.zi(), pointTwo.zi()));
         highPoint = new Pos(Math.max(pointOne.xi(), pointTwo.xi()), Math.max(pointOne.yi(), pointTwo.yi()), Math.max(pointOne.zi(), pointTwo.zi()));
     }
-    
+
     ////////////////////////////////////////////
     ///             Utility                  ///
     ////////////////////////////////////////////
-    
+
     /**
      * Replaces all blocks in the cube with a different type
+     *
      * @param world - world to manipulate
      * @param block - block to place
-     * @param meta - meta to place as
+     * @param meta  - meta to place as
      * @return list of location that were changed, used for Undo commands
      */
     public List<Pos> replaceBlocks(World world, Block block, int meta)
     {
         List<Pos> list = getBlockLocations(world, block, meta);
-        for(Pos Pos : list)
+        for (Pos Pos : list)
         {
             Pos.setBlock(world, block, meta);
         }
         return list;
     }
-    
-    /** Grabs all blocks in the cube
-     *  
+
+    /**
+     * Grabs all blocks in the cube
+     *
      * @param world - world to search in
      */
     public List<Pos> getBlockLocations(World world)
     {
         return getBlockLocations(world, null, -1, -1);
     }
-    
+
     /**
-     * Grabs all blocks in the cube that match 
+     * Grabs all blocks in the cube that match
+     *
      * @param world - world to search in
      * @param block - block instance to match against
      * @return list of blocks, never null but can be empty
@@ -135,44 +153,46 @@ public class Selection
     {
         return getBlockLocations(world, block, -1, -1);
     }
-    
+
     /**
-     * Grabs all blocks in the cube that match 
+     * Grabs all blocks in the cube that match
+     *
      * @param world - world to search in
      * @param block - block instance to match against
-     * @param meta - meta value to match
+     * @param meta  - meta value to match
      * @return list of blocks, never null but can be empty
      */
     public List<Pos> getBlockLocations(World world, Block block, int meta)
     {
         return getBlockLocations(world, block, meta, -1);
     }
-    
+
     /**
-     * Grabs all blocks in the cube that match 
+     * Grabs all blocks in the cube that match
+     *
      * @param world - world to search in
      * @param block - block instance to match against, if null will match all
-     * @param meta - meta value to match, if -1 will match all meta
-     * @param size - limiter for the list in case only a few blocks are wanted. 
-     *                  If zero or less will not limit size
+     * @param meta  - meta value to match, if -1 will match all meta
+     * @param size  - limiter for the list in case only a few blocks are wanted.
+     *              If zero or less will not limit size
      * @return list of blocks, never null but can be empty
      */
     public List<Pos> getBlockLocations(World world, Block block, int meta, int size)
     {
         List<Pos> list = new LinkedList<Pos>();
-        for(int y = lowPoint.yi(); y <= highPoint.yi(); y++ )
+        for (int y = lowPoint.yi(); y <= highPoint.yi(); y++)
         {
-            for(int x = lowPoint.xi(); x <= highPoint.xi(); x++ )
+            for (int x = lowPoint.xi(); x <= highPoint.xi(); x++)
             {
-                for(int z = lowPoint.zi(); z <= highPoint.zi(); z++ )
+                for (int z = lowPoint.zi(); z <= highPoint.zi(); z++)
                 {
-                    if(size > 0&& list.size() > size)
+                    if (size > 0 && list.size() > size)
                         return list;
-                    
+
                     Pos Pos = new Pos(x, y, z);
                     Block b = Pos.getBlock(world);
                     int m = Pos.getBlockMetadata(world);
-                    if(block == null || b == block && (meta == -1 || m == meta))
+                    if (block == null || b == block && (meta == -1 || m == meta))
                     {
                         list.add(Pos);
                     }
@@ -180,25 +200,79 @@ public class Selection
             }
         }
         return list;
-    }   
-    
+    }
+
+    /**
+     * Grabs all blocks near the point and within the distance.
+     *
+     * Note this search pattern does start at most negative corner
+     * TODO replace search pattern with same code the blasts use
+     * to select blocks in a bubble
+     *
+     * @param location - center point of the search
+     * @param size - number of items to return
+     * @param distance - distance to search
+     * @return list of locations of non air blocks sorted to closest to location
+     */
+    public List<Pos> getLocationsWithin(Location location, int size, int distance)
+    {
+        List<Pos> list = new LinkedList<Pos>();
+        if (distance > 0)
+        {
+            int min_y = (int) Math.max(lowPoint.yi(), location.y() - distance);
+            int max_y = (int) Math.min(highPoint.yi(), location.y() + distance);
+
+            int min_x = (int) Math.max(lowPoint.xi(), location.x() - distance);
+            int max_x = (int) Math.min(highPoint.xi(), location.x() + distance);
+
+            int min_z = (int) Math.max(lowPoint.zi(), location.z() - distance);
+            int max_z = (int) Math.min(highPoint.zi(), location.z() + distance);
+
+            for (int y = min_y; y <= max_y; y++)
+            {
+                for (int x = min_x; x <= max_x; x++)
+                {
+                    for (int z = min_z; z <= max_z; z++)
+                    {
+                        if (size > 0 && list.size() >= size)
+                        {
+                            Collections.sort(list, new Vector3DistanceComparator(location));
+                            return list;
+                        }
+
+                        Pos pos = new Pos(x, y, z);
+                        if (location.distance(pos) <= distance)
+                        {
+                            Block b = pos.getBlock(location.world());
+                            if (b != null && pos.isAirBlock(location.world()))
+                            {
+                                list.add(pos);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return list;
+    }
+
     ////////////////////////////////////////////
     ///  Field Getters                       ///
     ///             & Setters                ///
     ////////////////////////////////////////////
-    
+
     public void setPointOne(Pos Pos)
     {
         pointOne = Pos;
         recalc();
     }
-    
+
     public void setPointTwo(Pos Pos)
     {
         pointTwo = Pos;
         recalc();
-    }    
-    
+    }
+
     public Pos getLowPoint()
     {
         return lowPoint;
