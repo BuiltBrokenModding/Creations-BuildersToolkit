4b24fcab92769a36b7d98aa6dd8794bcb4376f93
Robert S <rseifert.phone@gmail.com>
2015-02-14 04:00:54 -0500

    Added .iml to ignore list, Changed Item glove to use IModeScrollItem handler for mode switching

--- src/main/java/com/builtbroken/creation/content/ItemGlove.java
+++ src/main/java/com/builtbroken/creation/content/ItemGlove.java
@@ -3,7 +3,7 @@ package com.builtbroken.creation.content;
 import com.builtbroken.creation.Creation;
 import com.builtbroken.creation.selection.Selection;
 import com.builtbroken.creation.selection.SelectionHandler;
-import com.builtbroken.mc.core.Engine;
+import com.builtbroken.mc.api.items.IModeItem;
 import com.builtbroken.mc.core.network.IPacketIDReceiver;
 import com.builtbroken.mc.core.network.packet.PacketPlayerItem;
 import com.builtbroken.mc.core.network.packet.PacketType;
@@ -23,20 +23,26 @@ import net.minecraft.world.World;
 import net.minecraftforge.client.event.MouseEvent;
 import net.minecraftforge.common.MinecraftForge;
 
+import java.util.List;
+
 /**
  * Created by robert on 1/25/2015.
  */
-public class ItemGlove extends Item implements IPacketIDReceiver
+public class ItemGlove extends Item implements IModeItem.IModeScrollItem
 {
-    public static final int MODE_PACKET = 0;
-
     protected int max_mode = 1;
     protected int min_mode = 0;
 
     public ItemGlove()
     {
         this.setMaxStackSize(1);
-        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    @Override @SideOnly(Side.CLIENT)
+    public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean b)
+    {
+        //TODO translate based on mode type
+        lines.add("Mode: " + getMode(stack));
     }
 
     @Override
@@ -85,32 +91,8 @@ public class ItemGlove extends Item implements IPacketIDReceiver
         }
     }
 
-    @SubscribeEvent @SideOnly(Side.CLIENT)
-    public void mouseHandler(MouseEvent e)
-    {
-        if (e.dwheel != 0)
-        {
-            EntityPlayer player = Minecraft.getMinecraft().thePlayer;
-            if (player.isSneaking())
-            {
-                ItemStack stack = player.getCurrentEquippedItem();
-                if (stack != null && stack.getItem() == this)
-                {
-                    cycleMode(stack, player, e.dwheel / 120);
-                    e.setCanceled(true);
-                }
-            }
-        }
-    }
-
-    /**
-     * Causes the mode to cycle
-     *
-     * @param stack  - itemstack
-     * @param player - player
-     * @param delta  - change in mode
-     */
-    protected void cycleMode(ItemStack stack, EntityPlayer player, int delta)
+    @Override
+    public int cycleMode(ItemStack stack, EntityPlayer player, int delta)
     {
         int n = getMode(stack) + delta;
         if (n > max_mode)
@@ -118,14 +100,11 @@ public class ItemGlove extends Item implements IPacketIDReceiver
         if (n < min_mode)
             n = max_mode;
 
-        if (getMode(stack) != n)
-        {
-            setMode(stack, n);
-            Engine.instance.packetHandler.sendToServer(new PacketPlayerItem(player, MODE_PACKET, n));
-        }
+        return n;
     }
 
-    protected int getMode(ItemStack stack)
+    @Override
+    public int getMode(ItemStack stack)
     {
         if (stack.getTagCompound() != null)
         {
@@ -134,29 +113,12 @@ public class ItemGlove extends Item implements IPacketIDReceiver
         return min_mode;
     }
 
-    protected void setMode(ItemStack stack, int mode)
+    @Override
+    public void setMode(ItemStack stack, int mode)
     {
         if (stack.getTagCompound() == null)
             stack.setTagCompound(new NBTTagCompound());
 
         stack.getTagCompound().setInteger("mode", mode);
-    }
-
-    @Override
-    public boolean read(ByteBuf buf, int id, EntityPlayer player, PacketType packet)
-    {
-        System.out.println("Packet: " + packet + "   ID: " + id);
-        if (packet instanceof PacketPlayerItem)
-        {
-            if (id == MODE_PACKET)
-            {
-                int slot = ((PacketPlayerItem) packet).slotId;
-                ItemStack stack = player.inventory.getStackInSlot(slot);
-                setMode(stack, buf.readInt());
-                player.addChatComponentMessage(new ChatComponentText("Mode set to " + getMode(stack)));
-                return true;
-            }
-        }
-        return false;
     }
 }
