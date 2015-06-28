1636f6a7dda4b341a69a4c973056ce1a657536c4
Robert Seifert <rseifert.phone@gmail.com>
2015-06-28 09:13:14 -0400

    Little work on the backend for installing and switching between installed modes for the glove

--- src/main/java/com/builtbroken/creation/content/glove/ItemGlove.java
+++ src/main/java/com/builtbroken/creation/content/glove/ItemGlove.java
@@ -2,6 +2,7 @@ package com.builtbroken.creation.content.glove;
 
 import com.builtbroken.creation.Creation;
 import com.builtbroken.creation.content.glove.modes.GloveMode;
+import com.builtbroken.jlib.lang.TextColor;
 import com.builtbroken.mc.api.items.IModeItem;
 import com.builtbroken.mc.core.registry.implement.IPostInit;
 import com.builtbroken.mc.lib.helper.LanguageUtility;
@@ -21,6 +22,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.world.World;
 
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -50,6 +52,8 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem, IPostI
     @SideOnly(Side.CLIENT)
     public void addInformation(ItemStack stack, EntityPlayer player, List lines, boolean b)
     {
+        if (stack.getItemDamage() == 1)
+            lines.add(TextColor.PURPLE.getColorString() + LanguageUtility.getLocal(getUnlocalizedName() + ".creative.name"));
         //TODO translate based on mode type
         lines.add(LanguageUtility.getLocal(getUnlocalizedName() + ".info.name"));
         lines.add(LanguageUtility.getLocal(getUnlocalizedName() + ".mode.name") + ": " + LanguageUtility.getLocal(getUnlocalizedName() + ".mode." + getMode(stack) + ".name"));
@@ -61,7 +65,6 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem, IPostI
     {
         getGloveMode(stack).onUpdate(stack, world, entity, slot, held);
     }
-
 
     @Override
     public boolean onEntitySwing(EntityLivingBase entityLiving, ItemStack stack)
@@ -114,22 +117,118 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem, IPostI
     @Override
     public int cycleMode(ItemStack stack, EntityPlayer player, int delta)
     {
-        //TODO check if the mode is installed
-        int n = getMode(stack) + delta;
-        if (n > max_mode)
-            n = 0;
-        if (n < 0)
-            n = max_mode;
-
-        return n;
+        if (delta == 0)
+            return getMode(stack);
+        if (stack.getTagCompound() != null && stack.getTagCompound().hasKey("modes") && stack.getTagCompound().getCompoundTag("modes").hasKey("list"))
+        {
+            int[] list = stack.getTagCompound().getCompoundTag("modes").getIntArray("list");
+            int n = getMode(stack) + delta;
+            if (n > list.length)
+                n = 0;
+            else if (n < 0)
+                n = list.length;
+            return n;
+        }
+        return 0;
     }
+
+    public boolean isModeInstalled(ItemStack stack, int mode)
+    {
+        if (mode == 0 || mode == 1)
+            return true;
+
+        if (mode >= 0 && mode < GloveModes.values().length)
+        {
+            return isModeInstalled(stack, GloveModes.values()[mode]);
+        }
+        return false;
+    }
+
+    public boolean isModeInstalled(ItemStack stack, GloveModes enum_mode)
+    {
+        //Remove this will cause an infinite loop in cycleMode
+        if (enum_mode == GloveModes.NONE || enum_mode == GloveModes.SELECTION)
+            return true; //Glove can always select an area
+
+        if (stack.getTagCompound() != null)
+        {
+            stack.setTagCompound(new NBTTagCompound());
+            if (stack.getTagCompound().hasKey("modes"))
+            {
+                //TODO change save format to allow modes to save data
+                NBTTagCompound tag = stack.getTagCompound().getCompoundTag("modes");
+                if (tag.hasKey(enum_mode.name().toLowerCase()))
+                {
+                    return tag.getInteger(enum_mode.name().toLowerCase()) > 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean installMode(ItemStack stack, int mode)
+    {
+        if (mode >= 0 && mode < GloveModes.values().length)
+        {
+            GloveModes enum_mode = GloveModes.values()[mode];
+            if (isModeInstalled(stack, enum_mode))
+            {
+                //TODO change save format to allow modes to save data
+                NBTTagCompound tag;
+                if (stack.getTagCompound().hasKey("modes"))
+                    tag = stack.getTagCompound().getCompoundTag("modes");
+                else
+                    tag = new NBTTagCompound();
+                if (!tag.hasKey(enum_mode.name().toLowerCase()))
+                {
+                    if (!tag.hasKey("list"))
+                    {
+                        tag.setIntArray("list", new int[]{0, 1, mode});
+                    }
+                    else
+                    {
+                        int[] pm = tag.getIntArray("list");
+                        int[] m = new int[pm.length + 1];
+                        m[pm.length] = mode;
+                        for (int i = 0; i < pm.length; i++)
+                        {
+                            m[i] = pm[i];
+                        }
+                        Arrays.sort(m);
+                        tag.setIntArray("list", m);
+                    }
+                    tag.setInteger(enum_mode.name().toLowerCase(), 1);
+                    stack.getTagCompound().setTag("modes", tag);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 
     public GloveMode getGloveMode(ItemStack stack)
     {
         int mode = getMode(stack);
-        if (mode >= 0 && mode < GloveModes.values().length)
+        if (stack.getTagCompound() != null)
         {
-            return GloveModes.values()[mode].mode;
+            if (!stack.getTagCompound().hasKey("modes"))
+            {
+                stack.getTagCompound().setTag("modes", new NBTTagCompound());
+            }
+            if (!stack.getTagCompound().getCompoundTag("modes").hasKey("list"))
+            {
+                stack.getTagCompound().getCompoundTag("modes").setIntArray("list", new int[]{0, 1});
+            }
+            int[] list = stack.getTagCompound().getCompoundTag("modes").getIntArray("list");
+            if (mode >= 0 && mode < list.length)
+            {
+                int actual_mode = list[mode];
+                if (actual_mode >= 0 && actual_mode < GloveModes.values().length)
+                {
+                    return GloveModes.values()[actual_mode].mode;
+                }
+            }
         }
         return GloveModes.NONE.mode;
     }
@@ -163,6 +262,19 @@ public class ItemGlove extends Item implements IModeItem.IModeScrollItem, IPostI
     public double getDurabilityForDisplay(ItemStack stack)
     {
         return GloveMode.getEnergy(stack) / max_energy;
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void getSubItems(Item item, CreativeTabs tab, List list)
+    {
+        list.add(new ItemStack(item, 1, 0));
+
+        ItemStack stack = new ItemStack(item, 1, 1);
+        for (GloveModes mode : GloveModes.values())
+        {
+            installMode(stack, mode.ordinal());
+        }
+        list.add(stack);
     }
 
 }
